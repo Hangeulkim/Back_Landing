5f1931119479f1d6d84a15d215f693c60d3b655f
Hangeulkim <ckswls89@naver.com>
2023-09-14 22:23:37 +0900

    component to config
    component to config

--- backland/src/main/java/osteam/backland/global/security/config/JwtAuthFilter.java
+++ backland/src/main/java/osteam/backland/global/security/config/JwtAuthFilter.java
@@ -6,18 +6,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import osteam.backland.global.attribute.Token;
 import osteam.backland.global.security.service.JwtTokenService;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthFilter extends OncePerRequestFilter {
--- backland/src/main/java/osteam/backland/global/security/config/LogFilter.java
+++ backland/src/main/java/osteam/backland/global/security/config/LogFilter.java
@@ -5,12 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 @Slf4j
 public class LogFilter extends OncePerRequestFilter {
     @Override
