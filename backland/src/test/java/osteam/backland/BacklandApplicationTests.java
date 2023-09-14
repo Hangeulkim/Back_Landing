8eb039ada35183a83674339a51ea4f11fc6cc70b
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-09-14 22:12:13 +0900

    Update BacklandApplicationTests.java

--- backland/src/test/java/osteam/backland/BacklandApplicationTests.java
+++ backland/src/test/java/osteam/backland/BacklandApplicationTests.java
@@ -3,7 +3,6 @@ package osteam.backland;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BacklandApplicationTests {
 
     @Test
