2b0e752969624cddcc6bc2149fadf901cbf06c84
Hangeulkim <ckswls89@naver.com>
2023-09-20 01:08:45 +0900

    hotfix/change-prefix
    hotfix/change-prefix

--- backland/src/main/java/osteam/backland/domain/person/controller/request/DeleteRequest.java
+++ backland/src/main/java/osteam/backland/domain/person/controller/request/DeleteRequest.java
@@ -1,10 +1,12 @@
 package osteam.backland.domain.person.controller.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class DeleteRequest {
     @NotNull
+    @Pattern(regexp = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$")
     public String phone;
 }
