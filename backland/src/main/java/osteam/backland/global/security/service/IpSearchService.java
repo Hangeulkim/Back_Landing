b0e17f2aa8228ebb2354350e59b2fd94128b6385
Hangeulkim <ckswls89@naver.com>
2023-09-25 16:33:55 +0900

    iptest
    iptest

--- backland/src/main/java/osteam/backland/global/security/service/IpSearchService.java
+++ backland/src/main/java/osteam/backland/global/security/service/IpSearchService.java
@@ -10,21 +10,6 @@ import java.util.Objects;
 @Slf4j
 public class IpSearchService {
 
-    private static final String[] IP_HEADER_CANDIDATES = {
-            "X-Real-IP",
-            "X-Forwarded-For",
-            "Proxy-Client-IP",
-            "WL-Proxy-Client-IP",
-            "HTTP_X_FORWARDED_FOR",
-            "HTTP_X_FORWARDED",
-            "HTTP_X_CLUSTER_CLIENT_IP",
-            "HTTP_CLIENT_IP",
-            "HTTP_FORWARDED_FOR",
-            "HTTP_FORWARDED",
-            "HTTP_VIA",
-            "REMOTE_ADDR"
-    };
-
     public static String getClientIP() {
 
 
@@ -33,13 +18,7 @@ public class IpSearchService {
         }
         // 현재 HTTP 요청에 대한 정보를 포함하고 있으며, 이를 통해 요청 헤더, 파라미터, 세션 등을 다룬다.
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        for (String header : IP_HEADER_CANDIDATES) {
-            String ipFromHeader = request.getHeader(header);
-            if (Objects.nonNull(ipFromHeader) && ipFromHeader.isEmpty() && !"unknown".equalsIgnoreCase(ipFromHeader)) {
-                String ip = ipFromHeader.split(",")[0];
-                return ip;
-            }
-        }
-        return request.getRemoteAddr();
+
+        return request.getHeader("X-Real-IP");
     }
 }
